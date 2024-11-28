dcb8b780ea5e88211c67406b5a9d8c7dbbf7e713
Alex <makargravanov@gmail.com>
2024-11-28 03:36:55 +0300

    add security config

---
+++ src/main/kotlin/ru/jetlabs/ts/userservice/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package ru.jetlabs.ts.userservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+/**
+ * Пустой конфиг, ничего не проверяющий, возможно, будет изменён в будущем
+ *
+ * @author Alex
+ */
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().permitAll()
+            }
+        return http.build()
+    }
+}
